da68f58030f63be1c538eecc11fa598310b2cfa5
Mart510 <martynandrewbillings@gmail.com>
2023-12-05 13:42:19 +0000

    auth redirect set up

--- intern/app/page.tsx
+++ intern/app/page.tsx
@@ -1,5 +1,20 @@
 /////// THIS IS THE DASHBOARD/MAIN HOMEPAGE //////////
+// import block
+import {createServerComponentClient} from '@supabase/auth-helpers-nextjs'
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+// utitly function for authentication check
+
 export default async function Index() {
+  // checking if the user is logged in
+  const supabase = createServerComponentClient({cookies});
+  const {data: {session},} = await supabase.auth.getSession();
+  // if not go to login page
+  if (!session) {
+    console.log('unauth user redirect to login')
+    redirect('/login');
+  }
+
   return (
     // adding redirects to either dashboard or login page depending on user status
     <section className="flex-1 w-full flex flex-col gap-20 items-center">
