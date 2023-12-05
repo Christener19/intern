cfb660350ab3311c85a555be762f8f2b0a31ece9
Ziad <ziadlabban@hotmail.co.uk>
2023-12-05 09:55:55 +0000

    cleanup done

--- intern/app/page.tsx
+++ intern/app/page.tsx
@@ -1,35 +1,8 @@
-import DeployButton from "../components/DeployButton";
-import AuthButton from "../components/AuthButton";
-import { createClient } from "@/utils/supabase/server";
-import ConnectSupabaseSteps from "@/components/ConnectSupabaseSteps";
-import SignUpUserSteps from "@/components/SignUpUserSteps";
-import Header from "@/components/Header";
-import { cookies } from "next/headers";
+/////// THIS IS THE DASHBOARD/MAIN HOMEPAGE //////////
 
 export default async function Index() {
-  const cookieStore = cookies();
-
-  // const canInitSupabaseClient = () => {
-  //   // This function is just for the interactive tutorial.
-  //   // Feel free to remove it once you have Supabase connected.
-  //   try {
-  //     createClient(cookieStore);
-  //     return true;
-  //   } catch (e) {
-  //     return false;
-  //   }
-  // };
-
-  // const isSupabaseConnected = canInitSupabaseClient();
-
   return (
     <section className="flex-1 w-full flex flex-col gap-20 items-center">
-      {/* <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-        <div className="w-full max-w-4xl flex justify-between items-center p-3 text-sm">
-          {/* <DeployButton /> */}
-      {/* {isSupabaseConnected && <AuthButton />} */}
-      {/* </div> */}
-      {/* </nav> */}
       <div>
         <h1>Hello World! Someone was here</h1>
       </div>
