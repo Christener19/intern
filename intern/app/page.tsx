ba6dc45b8e8a939eb40184ee022053157be83956
Mart510 <martynandrewbillings@gmail.com>
2023-12-05 11:33:17 +0000

    set up new branch for redirects

--- intern/app/page.tsx
+++ intern/app/page.tsx
@@ -1,6 +1,7 @@
 /////// THIS IS THE DASHBOARD/MAIN HOMEPAGE //////////
 export default async function Index() {
   return (
+    // adding redirects to either dashboard or login page depending on user status
     <section className="flex-1 w-full flex flex-col gap-20 items-center">
       <div>
         <h1>Hello World! Someone was here</h1>
