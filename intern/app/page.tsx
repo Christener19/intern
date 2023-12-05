6ec00df07a8457675c4945c9aa2c788ba524baf7
Ziad <ziadlabban@hotmail.co.uk>
2023-12-05 14:28:32 +0000

    dashboard skeleton

--- intern/app/page.tsx
+++ intern/app/page.tsx
@@ -18,9 +18,11 @@ export default async function Index() {
   return (
     // adding redirects to either dashboard or login page depending on user status
     <section className="flex-1 w-full flex flex-col gap-20 items-center">
-      <div>
-        <h1>Hello World! Someone was here</h1>
-      </div>
+        {/* < AttendanceTracker /> */}
+        {/* < EngagementLogger /> */}
+        {/* < ZoomPolls /> */}
+        {/* < RandomNamePicker /> */}
+    
     </section>
   );
 }
