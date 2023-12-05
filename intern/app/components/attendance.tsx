a1c7ba80214cac118e667751e3ad66d7c5aa4112
Mart510 <martynandrewbillings@gmail.com>
2023-12-05 16:14:34 +0000

    file spacing clean up

--- intern/app/components/attendance.tsx
+++ intern/app/components/attendance.tsx
@@ -20,42 +20,37 @@ import Image from 'next/image'
             //alert icon
         // alert box    
             
-
-
-
-
-
-
-
-        // div to hold everything
+    // div to hold everything
     export default function AttendanceTracker() {
             return (
-                <div>
+            <div>
                 <h1> Attendance tracking</h1> 
                  {/* // title */}
-<div> 
-    <h2> Todays attendance</h2>
-    <div> <p>22%</p></div>
-    <button> Download CSV</button>
-</div>
-{/* // div for attendance
-    // title
-    // attendance percentage/chart
-    // button download csv */}
+                <div> 
+                    <h2> Todays attendance</h2>
+                    <div> <p>22%</p></div>
+                    <button> Download CSV</button>
+                </div>
+            {/* // div for attendance
+                // title
+                // attendance percentage/chart
+                // button download csv */}
 
-<div>
-{/* // div for alerts */}
-<div>
-    <h2> Alerts</h2>
-    <Image />
-</div>
-    {/* //div for title
-    // div */}
-    <div id="alertbox">       </div>
-        
-          
-        </div> 
-        </div>  )
-        }
- 
+                <div>
+                {/* // div for alerts */}
+                    <div>
+                        <h2> Alerts</h2>
+                        <Image />
+                    </div>
+                {/* //div for title
+                // div */}
+                <div id="alertbox">
+
+                </div>
+                    
+                    </div> 
+            </div> 
+        )
+    }
             
+                        
