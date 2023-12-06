13e577488bf72e2651f396a47161420a7ab356f4
radswar <swar.rado@gmail.com>
2023-12-06 11:21:27 +0000

    grid branch

--- intern/app/page.tsx
+++ intern/app/page.tsx
@@ -1,32 +1,36 @@
 /////// THIS IS THE DASHBOARD/MAIN HOMEPAGE //////////
 // import block
-import {createServerComponentClient} from '@supabase/auth-helpers-nextjs'
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
-import NamePicker  from "../components/randomNamePicker"
-import { Montserrat } from "next/font/google"
+import NamePicker from "../components/randomNamePicker";
+import { Montserrat } from "next/font/google";
 
-const montserrat = Montserrat({subsets: ["latin"], variable: "--font-montserrat",})
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  variable: "--font-montserrat",
+});
 // utitly function for authentication check
 
 export default async function Index() {
   // checking if the user is logged in
-  const supabase = createServerComponentClient({cookies});
-  const {data: {session},} = await supabase.auth.getSession();
+  const supabase = createServerComponentClient({ cookies });
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
   // if not go to login page
   if (!session) {
-    console.log('unauth user redirect to login')
-    redirect('/login');
+    console.log("unauth user redirect to login");
+    redirect("/login");
   }
 
   return (
     // adding redirects to either dashboard or login page depending on user status
     <section className="flex-1 w-full flex flex-col gap-20 items-center">
-        {/* < AttendanceTracker /> */}
-        {/* < EngagementLogger /> */}
-        {/* < ZoomPolls /> */}
-        < NamePicker /> 
-    
+      {/* < AttendanceTracker /> */}
+      {/* < EngagementLogger /> */}
+      {/* < ZoomPolls /> */}
+      <NamePicker />
     </section>
   );
 }
