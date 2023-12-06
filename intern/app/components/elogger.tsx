f615e455787aa277476835c9be3b69ce34dd95ad
Humairaa Patel <humairaapatel@Humairaas-MacBook-Air.local>
2023-12-06 16:39:01 +0000

    elogger

---
+++ intern/app/components/elogger.tsx
@@ -0,0 +1,29 @@
+//title egagement logger
+//div search bar
+//div componate container  this can be state 
+    //new componant .map people cards  (back button)
+//back download CSV button
+//scroll auto
+//div form 
+// if card clicked profile appers
+import React from "react"
+import EngagementLoggerBox  from "./eloggerBox"
+
+export default function EngagementLogger() {
+    return (
+
+        <div>
+<h1>Engagement Logger</h1>
+    <div>
+        <input>Search</input>
+    </div>
+<div>
+<EngagementLoggerBox  EngagementInfo={EngagemnetData}/> 
+</div>
+<div>
+    <button>Download CSV</button>
+</div>
+
+        </div>
+    )
+}
