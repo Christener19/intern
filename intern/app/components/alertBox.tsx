73a03eab4ca33c9fcd0f05b4771e43a8d81e91d3
Christener19 <christenerleigh7070@gmail.com>
2023-12-06 10:05:11 +0000

    alertbox

--- intern/app/components/alertBox.tsx
+++ intern/app/components/alertBox.tsx
@@ -1,12 +1,15 @@
 import uuid from 'react-uuid'; 
+import AlertEntry from './alertEntry'; 
 
 export default function AlertBox({ alertInformation }) {
   return (
     // create alerts for every alert in the array
     <>
+    {alertInformation ? 
       {alertInformation.map((alert) => (
         <AlertEntry key={uuid()} infoalert={alert} />
-      ))}
+      ))} 
+      : null}
     </>
   );
 }
