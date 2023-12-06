0e7d3417feb347fbf12f40e74d728f9a325a434b
Christener19 <christenerleigh7070@gmail.com>
2023-12-06 14:57:40 +0000

    type never issue resolved

--- intern/app/components/alertBox.tsx
+++ intern/app/components/alertBox.tsx
@@ -2,7 +2,7 @@ import uuid from 'react-uuid';
 import AlertEntry from './alertEntry';
 import React from 'react';
 
-export default function AlertBox({ alertInformation }: { alertInformation: object| null }) {
+export default function AlertBox({ alertInformation }: { alertInformation: object[] | null }) {
   // if alert info is null or empty return nothing (blank state is good as no alerts)
   if (!alertInformation || alertInformation.length === 0) {
     return null;
@@ -10,7 +10,7 @@ export default function AlertBox({ alertInformation }: { alertInformation: objec
     return (
       // create alerts for every alert in the array
       <div className='rounded-md border-2 border-cyan-600 ml-4 mr-4 mb-4 h-48 box-border'>
-   {alertInformation.map((alert) => (
+         {alertInformation.map((alert : any) => (
           <AlertEntry key={uuid()} infoalert={alert} />
         ))}
       </div>
