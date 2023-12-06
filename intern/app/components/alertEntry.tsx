bf10cf520612652376d954201c2e49d256a593c0
Christener19 <christenerleigh7070@gmail.com>
2023-12-06 11:38:25 +0000

    debug info alert

--- intern/app/components/alertEntry.tsx
+++ intern/app/components/alertEntry.tsx
@@ -1,42 +1,43 @@
-export default function AlertEntry(alert) {
+export default function AlertEntry({ infoalert }) {
     // destructure for use in component
-    console.log(`alert`),
-    console.log(alert)
-    const {name, alertLevel} = alert
-console.log(`alertlevel: ${alert.alertLevel}`)
+    console.log('infoalert', infoalert);  
+    const { name, alertLevel } = infoalert;
+
+    console.log(`alertlevel: ${alertLevel}`);
+
     // set alert level
     let alertEmoji = null;
 
-    switch(alert.alertLevel) {
+    switch (alertLevel) {
         case 0:
             // no alert
             break;
         case 1:
             // level 1 - 1/2 day missing alert
-            alertEmoji = '⚠️'
+            alertEmoji = '⚠️';
             break;
         case 2:
             // level 2 - full day missing alert
-            alertEmoji = '⚠️⚠️📧'
+            alertEmoji = '⚠️⚠️📧';
             break;
         case 3:
-            // level 3 - 1.5 day missinng alert
-            alertEmoji = '⚠️⚠️⚠️📧'
+            // level 3 - 1.5 day missing alert
+            alertEmoji = '⚠️⚠️⚠️📧';
             break;
         default:
             // level 4 2 day missing alert
-            alertEmoji = '⚠️⚠️⚠️⚠️📧‼️'
+            alertEmoji = '⚠️⚠️⚠️⚠️📧‼️';
             break;
     }
 
     return (
         <>
-        <div className="flex flex-row text-lg">
-            {/* bootcamper name */}
-            <p>{name}</p>
-            {/* alert level */}
-            <p>{alertEmoji}</p>
-        </div>
+            <div className="flex flex-row text-lg">
+                {/* boot camper name */}
+                <p>{name}</p>
+                {/* alert level */}
+                <p>{alertEmoji}</p>
+            </div>
         </>
-    )
+    );
 }
