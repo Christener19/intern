5e09d94d94b08b65575a0de041e542beeeb1ef38
Christener19 <christenerleigh7070@gmail.com>
2023-12-07 14:02:20 +0000

    search bar

--- intern/app/components/elogger.tsx
+++ intern/app/components/elogger.tsx
@@ -1,3 +1,4 @@
+'use client';
 //title egagement logger
 //div search bar
 //div componate container  this can be state
@@ -5,22 +6,47 @@
 //back download CSV button
 //scroll auto
 //div form
-// if card clicked profile appers
-import React from "react";
+// if card clicked profile appersimport React, { useState } from "react";
 import EngagementLoggerBox from "./eloggerBox";
+import { useState } from "react";
 
+// Define the EngagementLogger component
 export default function EngagementLogger({ engagementData }: any) {
-  console.log('rendering EngagementLogger')
-  console.log(engagementData)
+  // State to manage the search term
+  const [searchTerm, setSearchTerm] = useState("");
+
+  // Event handler for updating the search term
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
+
+  // Filter the engagementData based on the search term
+  const filteredData = engagementData?.filter((person: any) =>
+    person.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  // Return the JSX for the component
   return (
     <div>
+      {/* Heading */}
       <h1>Engagement Logger</h1>
+
+      {/* Search input */}
       <div>
-        <input placeholder="Search" />
+        <input
+          type="text"
+          placeholder="Search"
+          value={searchTerm}
+          onChange={handleSearchChange}
+        />
       </div>
+
+      {/* EngagementLoggerBox component with filtered data */}
       <div>
-        <EngagementLoggerBox EngagementInfo={engagementData} />
+        <EngagementLoggerBox EngagementInfo={filteredData} />
       </div>
+
+      {/* Download CSV button */}
       <div>
         <button>Download CSV</button>
       </div>
