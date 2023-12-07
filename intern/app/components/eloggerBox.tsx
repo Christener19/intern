a5fc8331c4e6c0f62a988f7953478b2dbfac7080
Christener19 <christenerleigh7070@gmail.com>
2023-12-07 10:16:21 +0000

    engagment box

--- intern/app/components/eloggerBox.tsx
+++ intern/app/components/eloggerBox.tsx
@@ -1,3 +1,16 @@
-export default function EngagementLoggerBox () {
+import { v4 as uuidv4 } from 'uuid';
+import EngagementLogger from './elogger';
 
+export default function EngagementLoggerBox({ LoggerInfo }: { LoggerInfo: object[] | null }) {
+  if (!LoggerInfo) {
+    return null;
+  } else {
+    return (
+      <div>
+        {LoggerInfo.map((loggerPersonal: any) => (
+          <EngagementLogger key={uuidv4()} info={loggerPersonal} />
+        ))}
+      </div>
+    );
+  }
 }
