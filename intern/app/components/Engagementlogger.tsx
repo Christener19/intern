75a8dfa1679deae1047e1594e614b34d229e9664
Christener19 <christenerleigh7070@gmail.com>
2023-12-07 12:10:15 +0000

    useEffect hook for switch acse

--- intern/app/components/Engagementlogger.tsx
+++ intern/app/components/Engagementlogger.tsx
@@ -6,28 +6,29 @@
 //name
 //image/avatar
 //full data = objects -history
-//pass to sub show status
-import { useState } from "react";
-
+//pass to sub show statusimport React, { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 export default function EngagementLoggerCard({ cardObject }: any) {
   console.log("rendering card");
   console.log(cardObject);
 
   const [bgColor, setbgColor] = useState("bg-white-400");
 
-  const { name, avgEngagement, image, fullData } = cardObject;
+  useEffect(() => {
+    switch (cardObject.avgEngagement) {
+      case "good":
+        setbgColor("bg-green-400");
+        break;
+      case "poor":
+        setbgColor("bg-red-400");
+        break;
+      default:
+        setbgColor("bg-yellow-400");
+        break;
+    }
+  }, [cardObject.avgEngagement]);
 
-  switch (avgEngagement) {
-    case "good":
-      setbgColor("bg-green-400");
-      break;
-    case "poor":
-      setbgColor("bg-red-400");
-      break;
-    default:
-      setbgColor("bg-yellow-400");
-      break;
-  }
+  const { name } = cardObject;
 
   return (
     <div className={bgColor}>
@@ -35,7 +36,7 @@ export default function EngagementLoggerCard({ cardObject }: any) {
         {/* <Image src={image} height={64} width={64} alt={`profile picture for ${name}`} /> */}
         <div className="cardTextContent">
           <p>{name}</p>
-          <p>Average Engagement: Good</p>
+          <p>Average Engagement: {cardObject.avgEngagement}</p>
         </div>
       </div>
     </div>
