ea5fde4862d673e927e69bb6455009f6dd744919
radswar <swar.rado@gmail.com>
2023-12-07 09:40:01 +0000

    button work

--- intern/app/components/alertEntry.tsx
+++ intern/app/components/alertEntry.tsx
@@ -1,45 +1,46 @@
-export default function AlertEntry({ infoalert }) {
-    // destructure for use in component
-    console.log('infoalert', infoalert);  
-    const { name, alertLevel } = infoalert;
+export default function AlertEntry({ infoalert }: any) {
+  // destructure for use in component
+  console.log("infoalert", infoalert);
+  const { name, alertLevel } = infoalert;
 
-    console.log(`alertlevel: ${alertLevel}`);
+  console.log(`alertlevel: ${alertLevel}`);
 
-    // set alert level
-    let alertEmoji = null;
+  // set alert level
+  let alertEmoji: string | null;
+  alertEmoji = null;
 
-    switch (alertLevel) {
-        case 0:
-            // no alert
-            break;
-        case 1:
-            // level 1 - 1/2 day missing alert
-            alertEmoji = '⚠️';
-            break;
-        case 2:
-            // level 2 - full day missing alert
-            alertEmoji = '⚠️⚠️📧';
-            break;
-        case 3:
-            // level 3 - 1.5 day missing alert
-            alertEmoji = '⚠️⚠️⚠️📧';
-            break;
-        default:
-            // level 4 2 day missing alert
-            alertEmoji = '⚠️⚠️⚠️⚠️📧‼️';
-            break;
-    }
+  switch (alertLevel) {
+    case 0:
+      // no alert
+      break;
+    case 1:
+      // level 1 - 1/2 day missing alert
+      alertEmoji = "⚠️";
+      break;
+    case 2:
+      // level 2 - full day missing alert
+      alertEmoji = "⚠️⚠️📧";
+      break;
+    case 3:
+      // level 3 - 1.5 day missing alert
+      alertEmoji = "⚠️⚠️⚠️📧";
+      break;
+    default:
+      // level 4 2 day missing alert
+      alertEmoji = "⚠️⚠️⚠️⚠️📧‼️";
+      break;
+  }
 
-    return (
-        <>
-            <div className="flex flex-row justify-between">
-                {/* boot camper name */}
-                <p>{name}</p>
-                {/* alert level */}
-                <p>{alertEmoji}</p>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="flex flex-row justify-between">
+        {/* boot camper name */}
+        <p>{name}</p>
+        {/* alert level */}
+        <p>{alertEmoji}</p>
+      </div>
+    </>
+  );
 }
 
 // Removed form line 35 className="flex flex-row text-lg"
--- intern/app/components/attendance.tsx
+++ intern/app/components/attendance.tsx
@@ -1,6 +1,6 @@
 //import block
-import Image from 'next/image'
-import AlertBox from './alertBox'
+import Image from "next/image";
+import AlertBox from "./alertBox";
 
 ///////////////Attendance tracking page//////////////
 
@@ -8,59 +8,64 @@ import AlertBox from './alertBox'
 // title
 
 // div for attendance
-    // title
-    // attendance percentage/chart
-    // button download csv
-
+// title
+// attendance percentage/chart
+// button download csv
 
 // div for alerts
-    //div for title
-    //div for alerts
-        // div
-            //title
-            //alert icon
-        // alert box    
-            
-    // div to hold everything
-    export default function AttendanceTracker({attendanceAlert}) {
-    console.log(`got to attendance tracker`)
-    console.log(attendanceAlert)
+//div for title
+//div for alerts
+// div
+//title
+//alert icon
+// alert box
 
-    const {attPercent, alerts} = attendanceAlert 
+// div to hold everything
+export default function AttendanceTracker({ attendanceAlert }: any) {
+  console.log(`got to attendance tracker`);
+  console.log(attendanceAlert);
 
-    console.log(`attPercent: ${attPercent}`)
-    console.log(`alerts: ${alerts}`)
+  const { attPercent, alerts } = attendanceAlert;
 
+  console.log(`attPercent: ${attPercent}`);
+  console.log(`alerts: ${alerts}`);
 
-        const attendancePercent = {attPercent}
-            return (
-            <div className= "text-center w-full grid grid-rows-[auto,1fr] grid-flow-col gap-4  rounded-lg border-2 border-cyan-600 p-4 m-1 ">
-                <h1 className= "col-span-2 text-3xl  text-cyan-600 rounded-md h-fit" > Attendance tracking</h1> 
-                 {/* // title */}
-                <div className= "col-span-1 border-2 rounded-md border-cyan-600"> 
-            
-                    <h2 className= " text-2xl text-cyan-600"> Todays attendance</h2>
-                    <div id='attendance figure' className='rounded-md ml-4 mr-4 mb-b bg-cyan-600'> <p className='text-white text-9xl'>{attendancePercent.attPercent}</p></div>
-                    <button className='bg-green-500 rounded-md p-2 m-2 text-white'>Download CSV</button>
-                </div>
-            {/* // div for attendance
+  const attendancePercent = { attPercent };
+  return (
+    <div className="text-center w-full grid grid-rows-[auto,1fr] grid-flow-col gap-4  rounded-lg border-2 border-cyan-600 p-4 m-1 ">
+      <h1 className="col-span-2 text-3xl  text-cyan-600 rounded-md h-fit">
+        {" "}
+        Attendance tracking
+      </h1>
+      {/* // title */}
+      <div className="col-span-1 border-2 rounded-md border-cyan-600">
+        <h2 className=" text-2xl text-cyan-600"> Todays attendance</h2>
+        <div
+          id="attendance figure"
+          className="rounded-md ml-4 mr-4 mb-b bg-cyan-600"
+        >
+          {" "}
+          <p className="text-white text-9xl">{attendancePercent.attPercent}</p>
+        </div>
+        <button className="bg-green-500 rounded-md p-2 m-2 text-white">
+          Download CSV
+        </button>
+      </div>
+      {/* // div for attendance
                 // title
                 // attendance percentage/chart
                 // button download csv */}
 
-                <div className= " col-span-1 border-2 border-cyan-600 rounded-md" >
-                {/* // div for alerts */}
-                    <div>
-                        <h2 className=" text-2xl text-cyan-600"> Alerts</h2>
-                        <Image />
-                    </div>
-                {/* //div for title
-                // div */}
-                       <AlertBox  alertInformation={alerts}/> 
-                </div>
-            </div> 
-        )
-    }
-            
-                        
-    // This was from line 37 w-full justify-center  ml-auto mr-auto text-center
+      <div className=" col-span-1 border-2 border-cyan-600 rounded-md">
+        {/* // div for alerts */}
+        <div>
+          <h2 className=" text-2xl text-cyan-600"> Alerts</h2>
+          <Image src="/alarmbell.svg" width={24} height={24} alt="Alert icon" />
+        </div>
+        <AlertBox alertInformation={alerts} />
+      </div>
+    </div>
+  );
+}
+
+// This was from line 37 w-full justify-center  ml-auto mr-auto text-center
