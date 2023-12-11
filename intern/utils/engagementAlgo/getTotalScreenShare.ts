96ec352fd9ec1ff7ac4ec1db317921d00a87ac11
Wing-Kit Leung <wing_kitleung@hotmail.com>
2023-12-11 11:05:53 +0000

    started function for median screenshare time

--- intern/utils/engagementAlgo/getTotalScreenShare.ts
+++ intern/utils/engagementAlgo/getTotalScreenShare.ts
@@ -8,3 +8,31 @@
 // media -31% = 1pt
 
 // return score (1-3)
+
+function getTotalScreenShare(zoomID: number) {
+  console.log(`Getting Total Screen Share for ${zoomID}`);
+  let score = 0;
+  // Get call to look up Total Screen Share time
+  // Get call to look up cohort Screen Share time
+
+  let totalScreenShareTime = 0;
+  let cohortScreenShareArr = [2, 4, 5, 6];
+
+  function findMedian(cohortScreenShareArr: number[]) {
+    cohortScreenShareArr.sort((a, b) => a - b);
+    const middleIndex = Math.floor(cohortScreenShareArr.length / 2);
+
+    if (cohortScreenShareArr.length % 2 === 0) {
+      return (
+        (cohortScreenShareArr[middleIndex - 1] +
+          cohortScreenShareArr[middleIndex]) /
+        2
+      );
+    } else {
+      return cohortScreenShareArr[middleIndex];
+    }
+  }
+  console.log(cohortScreenShareArr);
+}
+
+getTotalScreenShare(34);
