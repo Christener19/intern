925a82512c573ac8444dd4c6c3c067841a5c9877
Mart510 <martynandrewbillings@gmail.com>
2023-12-11 09:43:24 +0000

    getTotalScreenShareSkeleton

---
+++ intern/utils/engagementAlgo/getTotalScreenShare.ts
@@ -0,0 +1,10 @@
+// getTotalScreenShare will takin in ZoomID
+// get call to look at screen share time for user and for all cohort
+// calc median screen share
+// calc break points
+// Switch
+// median -15% =  3pts
+// median -16% => -30% = 2pts
+// media -31% = 1pt
+
+// return score (1-3)
