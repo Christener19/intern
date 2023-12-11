7d762f0a3b092b0dc02bf7ec885060148f9dbba9
Wing-Kit Leung <wing_kitleung@hotmail.com>
2023-12-11 09:40:00 +0000

    get polls pseduo code

---
+++ intern/utils/engagementAlgo/getPolls.ts
@@ -0,0 +1,8 @@
+// getPolls function will take in ZoomId (?)
+// Get call to look at Zoom poll completion rate (poll-completion rate)
+// Look up
+// Return score (number 1-3)
+// Switch:
+// >90% = 3 pts
+// >=75-89 = 2 pts
+// <=74 = 1 pt
---
+++ intern/utils/engagementAlgo/getScoreForBootcamper.ts
@@ -0,0 +1,23 @@
+// getScoreForBootcamper  function to manage all scoring and returning of engagement data
+
+// getScoreForBootcamper func take ? argument (ZoomID)
+
+// Polls data
+// return the score
+
+// Total screen time
+// return the score
+
+// Freq of screen switch
+// return the score
+
+// Switch? or of statement
+
+// if score is =< 8
+// return Good
+
+// if score is < 6
+// return Average
+
+// else (score is lower than 6)
+// return Poor
