faa6b5659c9361c5d6758aab757567c5ee0db160
Wing-Kit Leung <wing_kitleung@hotmail.com>
2023-12-13 12:23:39 +0000

    started patch route, but problems

--- intern/pages/api/database/tests/controllers/attendanceController.ts
+++ intern/pages/api/database/tests/controllers/attendanceController.ts
@@ -5,43 +5,51 @@ import * as attendanceModel from "../models/attendanceModel";
 // api request handlers
 // Get all attendance
 export async function getBootcampers() {
-    try {
-        const todaysAttendance = await attendanceModel.getBootcampers();
-        return todaysAttendance;
-    } catch (error) {
-        console.error('Error in getBootcampers controller', error);
-    }
+  try {
+    const todaysAttendance = await attendanceModel.getBootcampers();
+    return todaysAttendance;
+  } catch (error) {
+    console.error("Error in getBootcampers controller", error);
+  }
 }
 
-// GET absent 
+// GET absent
 export async function getAbsentBootcampers() {
-    try {
-        const todaysAbsentees = await attendanceModel.getAbsentBootcampers();
-        return todaysAbsentees
-    } catch (error) {
-        console.error('Error in getAbsentBootcampers controller', error);
-    }
+  try {
+    const todaysAbsentees = await attendanceModel.getAbsentBootcampers();
+    return todaysAbsentees;
+  } catch (error) {
+    console.error("Error in getAbsentBootcampers controller", error);
+  }
 }
 
 // PATCH register
-export async function registerBootcamperAttendance(req : any, res : NextApiRequest) {
-    try {
-        const zoomId = req.query.zoomId;
-        const data = req.body
-        
-        // call registerBootcamperAttendance from model
-        const register = await attendanceModel.registerBootcamperAttendance(zoomId, data);
+export async function registerBootcamperAttendance(
+  req: any,
+  res: NextApiRequest
+) {
+  console.log("Calling registerBootcamperAttendance controller");
+  try {
+    // const zoomId = req.query.zoomId;
+    const data = req.body;
 
-        // assume 404 status if the zoomID is not found
-        if (!register) {
-            return {status: 'fail', data: {msg: 'ZoomId not found'} };
-        }
+    // console.log("zoomId", zoomId);
+    console.log(data);
+    // call registerBootcamperAttendance from model
+    const register = await attendanceModel.registerBootcamperAttendance(
+      zoomId,
+      data
+    );
 
-        // Return a success response
-        return { status: "success", data: register};
-
-    } catch (error) {
-        console.error('Error in registerBootcamperAttendance controller', error);
-        return {status: 'error', message: 'Internal server error'};
+    // assume 404 status if the zoomID is not found
+    if (!register) {
+      return { status: "fail", data: { msg: "ZoomId not found" } };
     }
+
+    // Return a success response
+    return { status: "success", data: register };
+  } catch (error) {
+    console.error("Error in registerBootcamperAttendance controller", error);
+    return { status: "error", message: "Internal server error" };
+  }
 }
--- intern/pages/api/database/tests/routes/PATCH/registerBootcamperAttendance.ts
+++ intern/pages/api/database/tests/routes/PATCH/registerBootcamperAttendance.ts
@@ -1,60 +1,50 @@
 // import block
-import { NextApiRequest, NextApiResponse } from 'next';
-import * as attendanceController from '../../controllers/attendanceController';
+import { NextApiRequest, NextApiResponse } from "next";
+import * as attendanceController from "../../controllers/attendanceController";
 
 // Main API route handler
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   const { method } = req;
 
-  // Handle GET request
-  if (method === 'GET') {
-    return handleGetRequest(req, res);
-  }
-
   // Handle PATCH request
-  if (method === 'PATCH') {
+  if (method === "PATCH") {
     return handlePatchRequest(req, res);
   }
 
   // If the method is not allowed, set the appropriate headers and status
-  res.setHeader('Allow', ['GET', 'PATCH']);
+  res.setHeader("Allow", ["PATCH"]);
   res.status(405).end(`Method ${method} Not Allowed`);
-}
-
-// Function to handle GET request
-async function handleGetRequest(req: NextApiRequest, res: NextApiResponse) {
-  try {
-    // Call the getBootcampers function from the controller
-    const todaysAttendance = await attendanceController.getBootcampers();
-    res.status(200).json({ status: 'success', data: todaysAttendance });
-  } catch (error) {
-    console.error('Error in getBootcampers:', error);
-    res.status(500).json({ status: 'error', message: 'Internal Server Error' });
-  }
 }
 
 // Function to handle PATCH request
 async function handlePatchRequest(req: any, res: NextApiResponse) {
   try {
     // Extract parameters from the request
-    const zoomId = req.query.id
+    const zoomId = req.query.id;
     const data = req.body;
 
-
     // Call the registerBootcamperAttendance function from the controller
-    const register = await attendanceController.registerBootcamperAttendance(zoomId, data);
+    const register = await attendanceController.registerBootcamperAttendance(
+      zoomId,
+      data
+    );
 
     // If the registration fails, return a 404 response
     if (!register) {
-      return res.status(404).json({ status: 'fail', data: { msg: 'ZoomId not found' } });
+      return res
+        .status(404)
+        .json({ status: "fail", data: { msg: "ZoomId not found" } });
     }
 
     // Return a success response
-    res.status(200).json({ status: 'success', data: register });
+    res.status(200).json({ status: "success", data: register });
   } catch (error) {
     // Handle errors and return a 500 response
-    console.error('Error in registerBootcamperAttendance:', error);
-    res.status(500).json({ status: 'error', message: 'Internal Server Error' });
+    console.error("Error in registerBootcamperAttendance:", error);
+    res.status(500).json({ status: "error", message: "Internal Server Error" });
   }
 }
 
