32ddfaa01a0018b8308941378ee1833c820b78da
Mart510 <martynandrewbillings@gmail.com>
2023-12-13 15:25:20 +0000

    got csv downloads working

--- intern/pages/api/database/tests/routes/downloads/downloadAttendance.ts
+++ intern/pages/api/database/tests/routes/downloads/downloadAttendance.ts
@@ -1,25 +1,25 @@
 // Import block
-
 import { NextApiRequest, NextApiResponse } from "next";
 import pool from "../../../dbIndex";
-import createCsvWriter from "csv-writer";
-import { create } from "ts-node";
+import { parse } from 'json2csv';
 
-export default async function csvDownloader(req: NextApiRequest, res: NextApiResponse) {
-    const { rows } = await pool.query(`SELECT * FROM test_attendance`);
-
-    if (rows.length === 0) {
-        return res.status(404).json({error: 'No data found' });
+export default async function handler(req : NextApiRequest, res : NextApiResponse) {
+    try {
+      // Fetch data from the test_attendance table
+      const result = await pool.query('SELECT * FROM test_attendance');
+      const data = result.rows;
+  
+      // Convert the json data to CSV using json2csv
+      const csvData = parse(data, { header: true });
+  
+      // Set the headers for the response and the file name
+      res.setHeader('Content-Type', 'text/csv');
+      res.setHeader('Content-Disposition', 'attachment; filename=attendance.csv');
+  
+      // Send the CSV data as the response
+      res.status(200).send(csvData);
+    } catch (error) {
+      console.error('Error fetching data from the database:', error);
+      res.status(500).end();
     }
-
-    const csvWriter = createCsvWriter({
-        path: 'ferrari.csv',
-        header: Object.keys(rows[0]).map((key) => ({ id:key, title: key }))
-    });
-
-    csvWriter.writeRecords(rows).then(() => {
-        res.setHeader('Content-Type', 'text/csv');
-        res.setHeader('Content-Disposition', 'attachment; filename=ferrari.csv');
-        res.status(200).download('ferrari.csv')
-    });
-}
+  }
