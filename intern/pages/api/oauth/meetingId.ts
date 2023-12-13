b108d7705fc0af725b035fb76a74d1ef2e41b5a2
Wing-Kit Leung <wing_kitleung@hotmail.com>
2023-12-13 08:49:19 +0000

    loop to gather relevant data

--- intern/pages/api/oauth/meetingId.ts
+++ intern/pages/api/oauth/meetingId.ts
@@ -71,7 +71,77 @@ export default async function handler(
 
     // Extract the participants data from the response
     const participantsData = await participantsResponse.json();
-    console.log("Meeting Participants:", participantsData.participants);
+    // console.log("Meeting Participants:", participantsData.participants);
+
+    const registrantIDs = participantsData.participants.map(
+      (participant) => participant.registrant_id
+    );
+
+    const uniqueRegistrantIDs = registrantIDs.filter(function (
+      value,
+      index,
+      self
+    ) {
+      return self.indexOf(value) === index && value !== undefined;
+    });
+
+    interface newParticipantObjType {
+      registrant_id: string;
+      name: string;
+      user_email: string;
+      day: string;
+      duration: number;
+      join_leave: JoinLeaveType[];
+    }
+
+    interface JoinLeaveType {
+      join: Date;
+      leave: Date;
+    }
+
+    let newParticipantArr = [];
+
+    for (let j = 0; j < uniqueRegistrantIDs.length; j++) {
+      let duration = 0;
+      let newParticipantObj: newParticipantObjType = {
+        registrant_id: "",
+        name: "",
+        user_email: "",
+        day: "",
+        duration: 0,
+        join_leave: [],
+      };
+
+      for (let i = 0; i < participantsData.participants.length; i++) {
+        if (
+          participantsData.participants[i].registrant_id ===
+          uniqueRegistrantIDs[j]
+        ) {
+          duration = duration + participantsData.participants[i].duration;
+          newParticipantObj.registrant_id =
+            participantsData.participants[i].registrant_id;
+          newParticipantObj.name = participantsData.participants[i].name;
+          newParticipantObj.user_email =
+            participantsData.participants[i].user_email;
+          newParticipantObj.day = participantsData.participants[
+            i
+          ].join_time.slice(0, 11);
+          let newJoinLeave = {
+            join: participantsData.participants[i].join_time,
+            leave: participantsData.participants[i].leave_time,
+          };
+          newParticipantObj.join_leave.push(newJoinLeave);
+        }
+        newParticipantObj.duration = duration;
+      }
+      newParticipantArr.push(newParticipantObj);
+
+      console.log("new Array:", newParticipantArr);
+      console.log("Join_leave Array:");
+      console.log(newParticipantArr[0].join_leave[0]);
+      console.log(newParticipantArr[0].join_leave[1]);
+      console.log(newParticipantArr[0].join_leave[2]);
+    }
 
     // Send the participants data in the response
     res.status(200).json({ success: true, participants: participantsData });
