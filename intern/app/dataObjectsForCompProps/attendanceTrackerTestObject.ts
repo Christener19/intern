2e972710c0b74f469a07d56d52fbc5f7afad891b
Mart510 <martynandrewbillings@gmail.com>
2023-12-14 13:50:10 +0000

    attendance working

--- intern/app/dataObjectsForCompProps/attendanceTrackerTestObject.ts
+++ intern/app/dataObjectsForCompProps/attendanceTrackerTestObject.ts
@@ -8,27 +8,29 @@ export const testAttPerecent = async (): Promise<object> => {
     // console.log(`mainRoute = ${mainRoute()}`)
     // console.log(`getRoute = ${getRoute}`)
 
-    // get the total number of bootcampers attending today
+    // // get the total number of bootcampers attending today
+    // const bootCampersPresentResponse = await fetch(`${baseURL}${getRoute}getBootcampers`);
+    // const bootCampersPresent = await bootCampersPresentResponse.text();
+
+    // do call get data
     const bootCampersPresentResponse = await fetch(`${baseURL}${getRoute}getBootcampers`);
+    // clean up JSON
     const bootCampersPresent = await bootCampersPresentResponse.text();
     const bootCampersPresentObject = JSON.parse(bootCampersPresent)
-    const presentCount = bootCampersPresentObject.data
 
-    // get the total number of missing bootcampers today
-    const bootCampersAbsentResponse = await fetch(`${baseURL}${getRoute}getAbsentBootcampers`);
-    const bootCampersAbsent = await bootCampersAbsentResponse.text();
-    const bootCampersAbsentObject = JSON.parse(bootCampersAbsent)
-
+    // break data into variables we need
+    const totalPresent = Number(bootCampersPresentObject.data.todaysAttendanceCount)
+    const totalMissing = Number(bootCampersPresentObject.data.todaysAbsentCount)
+    // // get the total number of missing bootcampers today
+    const totalCount = totalPresent + totalMissing
 
     // debug loggers
-    console.log(`bootCampersPresentObject`)
-    console.log(bootCampersPresentObject)
-    console.log(`bootCampersAbsentObject`)
-    console.log(bootCampersAbsentObject)
-
+    // console.log(`totalPresent = ${totalPresent}`)
+    // console.log(`totalMissing = ${totalMissing}`)
+    // console.log(`totalCount = ${totalCount}`)
 
     // Calculate attendance percentage
-    const attendancePercentage = (bootCampersPresent / (bootCampersAbsent + bootCampersPresent)) * 100;
+    const attendancePercentage = (totalPresent / (totalCount) * 100);
 
     const cleanAttPercent = attendancePercentage.toFixed(2);
 
@@ -41,7 +43,7 @@ export const testAttPerecent = async (): Promise<object> => {
                 { name: 'Dave', alertLevel: 97 },
             ]
             };
-}
+};
 
 // export const testAttendanceData = {
 //     attPercent: await testAttPerecent(), 
