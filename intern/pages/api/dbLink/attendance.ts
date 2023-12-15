99f36f6c8d3d7c05b8d64e55230406fbaf28d0e8
Wing-Kit Leung <wing_kitleung@hotmail.com>
2023-12-15 09:57:55 +0000

    comments added - psuedo code

--- intern/pages/api/dbLink/attendance.ts
+++ intern/pages/api/dbLink/attendance.ts
@@ -3,10 +3,33 @@ import { mainRoute, getRoute, oauthRoute } from "@/utils/APIRouteSetter";
 
 const mainURL = mainRoute();
 
+// Function to grab participant attendance data from Zoom API and add to our database
 export default async function attendanceZoomToDb() {
+  // Grab zoom data of everyone, latest meeting ID
   const participants = fetch(`${mainURL}${oauthRoute}getMeetingParticipants`);
 
+  // debug logger
   console.log(participants);
+
+  // Try and catch for zoomID
+  // If no zoomid then insert new row
+  // If zoomid then do following:
+
+  // Format information into this shape:
+  // Raw data from zoom:
+  // todays_attendance_hours = COALESCE ($1, todays_attendance_hours),
+  // WHERE zoomId = $5
+
+  // Convert duration (seconds) to hours.
+
+  // Get data from database
+
+  // Calculated from DB + Zoom
+  // total_attendance_hours = COALESCE ($2, total_attendance_hours),
+  // total_days_attended = COALESCE ($3, total_days_attended),
+  // missing_streak = COALESCE ($4, missing_streak)
+
+  // Create loop to patch by zoomid.
 
   const getBootcampers = fetch(
     "http://localhost:3000/api/database/route/GET/getBootcampers"
