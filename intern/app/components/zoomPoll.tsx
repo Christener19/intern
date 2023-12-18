33030f38e750e4471250e4bcbb91cb243bee97f0
Christener19 <christenerleigh7070@gmail.com>
2023-12-18 14:02:05 +0000

    vercel happy

--- intern/app/components/zoomPoll.tsx
+++ intern/app/components/zoomPoll.tsx
@@ -12,33 +12,28 @@ import { fetchZoomPollResults } from "../dataObjectsForCompProps/zoompollTestObj
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, Tooltip);
 
+
+// Type definition for the component's props
 type ZoomPollsProps = {
-  zoomPollID?: number;
+  zoomPollData?: { good: number; average: number; poor: number };
 };
 
-const ZoomPolls: React.FC<ZoomPollsProps> = ({ zoomPollID }) => {
-  const [pollResults, setPollResults] = useState({
-    good: 0,
-    average: 0,
-    poor: 0,
-  });
+const ZoomPolls: React.FC<ZoomPollsProps> = ({ zoomPollData }) => {
+  const [pollResults, setPollResults] = useState({ good: 0, average: 0, poor: 0 });
   const [showResults, setShowResults] = useState(false);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const results = await fetchZoomPollResults(zoomPollID);
-      if (results) {
-        // Extract only the necessary fields
-        const { good, average, poor } = results;
-        setPollResults({ good, average, poor });
-        setShowResults(true);
-      } else {
-        setShowResults(false);
-      }
-    };
-
-    fetchData();
-  }, [zoomPollID]);
+    if (zoomPollData) {
+      setPollResults({ 
+        good: zoomPollData.good, 
+        average: zoomPollData.average, 
+        poor: zoomPollData.poor 
+      });
+      setShowResults(true);
+    } else {
+      setShowResults(false);
+    }
+  }, [zoomPollData]);
 
   const chartData = {
     labels: ["Good", "Average", "Poor"],
@@ -72,7 +67,7 @@ const ZoomPolls: React.FC<ZoomPollsProps> = ({ zoomPollID }) => {
         },
         cornerRadius: 4,
         displayColors: true,
-        mode: "index",
+        mode: "index" as const,
         intersect: false,
       },
     },
@@ -121,86 +116,3 @@ const ZoomPolls: React.FC<ZoomPollsProps> = ({ zoomPollID }) => {
 };
 
 export default ZoomPolls;
-
-// // console.log(zoomPollData[0].questions[0].question_details[0].answer);
-
-// let good = 0;
-// let neutral = 0;
-// let bad = 0;
-
-// // function countPollAnswers(answer) {
-// //   zoomPollData.map((answer)=> (zoomPollData[0].questions[0].question_details[0].answer))
-// // }
-
-// function countPollAnswers(zoomPollData: any) {
-//   let good = 0;
-//   let neutral = 0;
-//   let bad = 0;
-//   for (let i = 0; i < zoomPollData.length; i++) {
-//     if (zoomPollData[i].questions[0].question_details[0].answer === "Good") {
-//       good++;
-//     }
-//     if (zoomPollData[i].questions[0].question_details[0].answer === "Bad") {
-//       bad++;
-//     }
-//     if (zoomPollData[i].questions[0].question_details[0].answer === "Neutral") {
-//       neutral++;
-//     }
-//   }
-//   return { good, neutral, bad };
-// }
-
-// let thermometerData: data = countPollAnswers(zoomPollData);
-// interface data {
-//   good: number;
-//   neutral: number;
-//   bad: number;
-// }
-
-// console.log(Object.values(thermometerData));
-
-// type ZoomPollsProps = {
-//   // Define props here if necessary
-// };
-
-// const chartData = {
-//   labels: Object.keys(thermometerData),
-//   datasets: [
-//     {
-//       label: "Thermometer",
-//       data: Object.values(thermometerData),
-//       backgroundColor: ["green", "blue", "red"],
-//     },
-//   ],
-// };
-
-// const ZoomPolls: React.FC<ZoomPollsProps> = (props) => {
-//   const [showResults, setShowResults] = useState(false);
-
-//   return (
-//     <div className="border-2 border-blue-500 p-4 rounded-xl shadow-sm w-full h-full mt-1 mr-2">
-//       <div className="text-center text-xl text-blue-500 font-bold mb-4">
-//         <h2>ZOOM POLLS</h2>
-//       </div>
-//       <div className="bg-gray-200 p-4 flex justify-around items-end h-52 rounded-xl">
-//         {showResults ? (
-//           <Bar data={chartData} />
-//         ) : (
-//           <div className="text-center w-full text-black mb-20 uppercase font-semibold">
-//             No results
-//           </div>
-//         )}
-//       </div>
-//       <div className="flex justify-center mt-4">
-//         <button
-//           className="bg-green-500 hover:bg-green-600 text-white py-2 px-4 border-none cursor-pointer rounded-xl shadow-sm uppercase font-bold mt-4"
-//           onClick={() => setShowResults(!showResults)} // Toggle the showResults state
-//         >
-//           Thermometer
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default ZoomPolls;
