3cc621545d92a81a2d0644e34a0b61074bb79cf4
Mart510 <martynandrewbillings@gmail.com>
2023-12-18 09:11:57 +0000

    allscreendatafetcher working, now to do the patch

--- intern/app/dataObjectsForCompProps/engagementLoggerPipeline.ts
+++ intern/app/dataObjectsForCompProps/engagementLoggerPipeline.ts
@@ -9,34 +9,50 @@ import findMedian from '../../utils/engagementAlgo/findMedian'
 // GET the data points from the database
 async function allScreenDataFetcher(weekNumber: number) {
 
-    console.log(weekNumber)
-    console.log(`allScreen URL: ${baseURL}${getRoute}getAllScreenData?weeknumber=${weekNumber}`)
-    console.log(`allBootcampers URL: ${baseURL}${getRoute}getAllScreenData?weeknumber=${weekNumber}`)
+    // debug loggers
+    // console.log(weekNumber)
+    // console.log(`allScreen URL: ${baseURL}${getRoute}getAllScreenData?weeknumber=${weekNumber}`)
+    // console.log(`allBootcampers URL: ${baseURL}${getRoute}getAllScreenData?weeknumber=${weekNumber}`)
 
     // Freq Screen Switch for all as an array
     // Total screen share for all as an array
     console.log('getting all screen data')
     const allScreenData = await fetch(`${baseURL}${getRoute}getAllScreenData?weeknumber=${weekNumber}`)
     // clean up response json
-    interface screenData {
-        allScreenTime: {screen_share_time: number}[]
-        allScreenSwitch: {screen_share_switch_freq: number}[]
-    }
+    // interface screenData {
+    //     allScreenTime: {screen_share_time: number}[]
+    //     allScreenSwitch: {screen_share_switch_freq: number}[]
+    // }
     const allScreenDataJSON = await allScreenData.text();
-    const allScreenDataClean: screenData = JSON.parse(allScreenDataJSON)
-    console.log(allScreenData)
+    const allScreenDataClean = JSON.parse(allScreenDataJSON)
+    const allScreenDataPayload = allScreenDataClean.data.data
+    // console.log(allScreenDataPayload) // debug logger
 
     // deconstruct json object into the two arrays
-    const allScreenTimeArr: number[] = allScreenDataClean.allScreenTime.map(singleValue => singleValue['screen_share_time'])
-    const allScreenSwitchArr: number[] = allScreenDataClean.allScreenSwitch.map(singleValue => singleValue['screen_share_time'])
+    const allScreenTimeArr: number[] = allScreenDataPayload.allScreenTime.map(singleValue => singleValue['screen_share_time'])
+    const allScreenSwitchArr: number[] = allScreenDataPayload.allScreenSwitch.map(singleValue => 
+    singleValue['screen_share_switch_freq'])
+    // debug loggers
+    // console.log('allScreenTimeArr')
+    // console.log(allScreenTimeArr)
+    // console.log('allScreenSwitchArr')
+    // console.log(allScreenSwitchArr)
 
     // Calculate median for each
     const screenShareMedian = findMedian(allScreenTimeArr)
     const screenSwitchMedian = findMedian(allScreenSwitchArr)
+    // debug loggers
+    // console.log(`screenShareMedian: ${screenShareMedian}`)
+    // console.log(`screenSwitchMedian: ${screenSwitchMedian}`)
 
     // Calculate the breakpoints () from the medians
-    const screenSwitchFreqBreakPoint = findBreakPoints(0.2, 0.4, screenSwitchMedian)
     const screenShareBreakPoint = findBreakPoints(0.15, 0.3, screenShareMedian)
+    const screenSwitchFreqBreakPoint = findBreakPoints(0.2, 0.4, screenSwitchMedian)
+    // debug loggers
+    // console.log(`screenShareBreakPoint`)
+    // console.log(screenShareBreakPoint)
+    // console.log(`screenSwitchFreqBreakPoint}`)
+    // console.log(screenSwitchFreqBreakPoint)
 
     // get all bootcampers and clean up into array of objects
     const allBootcampers = await fetch(`${baseURL}${getRoute}getBootcampersForEngagement?weeknumber=${weekNumber}`)
@@ -48,11 +64,12 @@ async function allScreenDataFetcher(weekNumber: number) {
     }
     const allBootcampersJSON = await allBootcampers.text();
     const allBootcampersArr = JSON.parse(allBootcampersJSON);
+    const allBootcampersPayload = allBootcampersArr.data.data
     console.log('bootcamperArr check')
-    console.log(allBootcampersArr);
+    //console.log(allBootcampersPayload);
     // restructure it into the right format
-    const bootcampers = allBootcampersArr.data.map(entry => ({
-        zoomID: Number(entry.zoomId),
+    const bootcampers: bootcamperData = allBootcampersPayload.map(entry => ({
+        zoomID: Number(entry.zoomid),
         screenShareTotal: Number(entry.screen_share_time),
         screenSwitchTotal: Number(entry.screen_share_switch_freq),
         pollCompletionRate: Number(entry.poll_completion_rate)
@@ -60,15 +77,15 @@ async function allScreenDataFetcher(weekNumber: number) {
 
     console.log(bootcampers)
     // Create return object
-    // return {
-    //     // array of objects of each bootcamper {id, screenshareData, screenSwitchData, pollCompletionRate}
-    //     bootcampers,
-    //     // breakpoints {ScreenShare, ScreenSwitch}
-    //     breakpoints: {
-    //         screenShareBreakPoint,
-    //         screenSwitchFreqBreakPoint
-    //     }
-    // }
+    return {
+        // array of objects of each bootcamper {id, screenshareData, screenSwitchData, pollCompletionRate}
+        bootcampers,
+        // breakpoints {ScreenShare, ScreenSwitch}
+        breakpoints: {
+            screenShareBreakPoint,
+            screenSwitchFreqBreakPoint
+        }
+    }
 } ;
 
 allScreenDataFetcher(1)
