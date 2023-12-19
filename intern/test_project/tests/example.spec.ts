cf9d1e22e9a6a57e2b37daa672a47b6a48714ff4
Christener19 <christenerleigh7070@gmail.com>
2023-12-19 17:08:00 +0000

    playwrght passes login and goes to dahsboard

--- intern/test_project/tests/example.spec.ts
+++ intern/test_project/tests/example.spec.ts
@@ -1,10 +1,27 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from '@playwright/test';
+require('dotenv').config({ path: '../.env.local' });
 
-test("should pick a random name", async ({ page }) => {
-  // Navigate to the page where the NamePicker component is rendered
-  await page.goto("http://localhost:3000/");
 
-  // Click the "Pick" button
-  await page.click("text=Login");
 
+test('should pick a random name', async ({ page }) => {
+  // Navigate to the login page
+  await page.goto('http://localhost:3000/login');
+
+  // Retrieve environment variables
+  const MY_EMAIL = process.env.MY_EMAIL!;
+  const MY_PASSWORD = process.env.MY_PASSWORD!;
+
+  // Fill in the email and password fields
+  await page.fill('input[name="email"]', MY_EMAIL);
+  await page.fill('input[name="password"]', MY_PASSWORD);
+
+  // Click the "Login" button
+  await page.click('button:has-text("Login")');
+
+  // Wait for navigation after login
+  await page.waitForNavigation();
+
+  // Navigate to the homepage
+  await page.goto('http://localhost:3000/');
 });
+
