95115111608851f0625bcb531ad034cf723ee1d2
Mart510 <martynandrewbillings@gmail.com>
2023-12-21 14:27:05 +0000

    trying to configure ts-jest

--- intern/jest.config.js
+++ intern/jest.config.js
@@ -2,4 +2,5 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: ['unit_testing/api_unit.test.ts'],
 };
---
+++ intern/unit_testing/api_unit.test.ts
@@ -0,0 +1,10 @@
+// import block
+import request from 'supertest';
+import App from 'next/app';
+import * as tsJest from 'ts-jest';
+
+// blank test to confirm setup is working
+
+test('Hi mom!', function() {
+    // this is blank just to ensure ts-jest is configured correctly
+});
