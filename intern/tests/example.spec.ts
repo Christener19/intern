6218d07a7622c4a4f25d3f0315f7f997a8f2a5fd
radswar <swar.rado@gmail.com>
2023-12-21 11:54:54 +0000

    hello

---
+++ intern/tests/example.spec.ts
@@ -0,0 +1,58 @@
+import { test, expect } from "@playwright/test";
+require("dotenv").config({ path: "../.env.local" });
+
+test("login", async ({ page }) => {
+  // Navigate to the login page
+  await page.goto("http://localhost:3000/login");
+
+  // Retrieve environment variables
+  const MY_EMAIL = process.env.MY_EMAIL!;
+  const MY_PASSWORD = process.env.MY_PASSWORD!;
+
+  // Fill in the email and password fields
+
+  await page.fill('input[name="email"]', "sarahmontegro@gmail.com");
+  await page.fill('input[name="password"]', "radek480");
+
+  // Click the "Login" button
+  await page.click('button:has-text("Login")');
+
+  // Wait for navigation after login
+  await page.waitForNavigation();
+
+  // Navigate to the homepage
+  await page.goto("http://localhost:3000/");
+
+  // Click the "Pick" button
+  await page.click('button:has-text("Pick")');
+
+  // Wait for the selected name to be updated
+  await page.waitForFunction(() => {
+    const element = document.querySelector(".bg-blue-500.text-white");
+    // Check both for the existence of the element and that its textContent is not null and not empty
+    return element?.textContent?.trim() !== "";
+  });
+
+  // Retrieve the selected name
+  const selectedName = await page.textContent(".bg-blue-500.text-white");
+  expect(selectedName).not.toBe("");
+
+  //Zoom polls
+  await page.click('button:has-text("Thermometer")');
+
+  await page.waitForFunction(
+    () => {
+      const chart = document.querySelector(
+        '[data-testid="zoom-poll-chart"] canvas'
+      );
+      return chart !== null;
+    },
+    null,
+    { timeout: 10000 }
+  );
+
+  const isChartVisible = await page.isVisible(
+    '[data-testid="zoom-poll-chart" canvas]'
+  );
+  expect(isChartVisible).toBe(true);
+});
