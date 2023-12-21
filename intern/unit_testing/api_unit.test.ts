b963e01c326789954e17b93d87d74c2fcf18f2a7
Wing-Kit Leung <wing_kitleung@hotmail.com>
2023-12-21 16:15:04 +0000

    get engagement score finished

--- intern/unit_testing/api_unit.test.ts
+++ intern/unit_testing/api_unit.test.ts
@@ -20,25 +20,64 @@ test("Hi mom!", function () {
 // Get Attendance test
 test("Get attenance test", async function () {
   // Reset db
-//   await resetTestDatabase();
+  //   await resetTestDatabase();
   // Run API call
   const testReply = await fetch(`${mainURL}${getRoute}getBootcampers`);
   // clean up response
-    // clean up response json
-    const testReplyJSON = await testReply.text();
-    const testReplyclean = JSON.parse(testReplyJSON);
-    // console.log(testReplyclean)
+  // clean up response json
+  const testReplyJSON = await testReply.text();
+  const testReplyclean = JSON.parse(testReplyJSON);
+  // console.log(testReplyclean)
   // Check response object is type of
-  expect(testReplyclean).toHaveProperty("data", {"todaysAbsentCount": "3", "todaysAttendanceCount": "7"});
+  expect(testReplyclean).toHaveProperty("data", {
+    todaysAbsentCount: "3",
+    todaysAttendanceCount: "7",
+  });
   // Check success status
-  expect(testReplyclean).toHaveProperty('status', 'success');
+  expect(testReplyclean).toHaveProperty("status", "success");
   // Check data object
-  expect(testReplyclean.data).not.toHaveProperty('todays_Attendance_Count', '64')
-  expect(testReplyclean.data).not.toHaveProperty('todays_Absent_Count', 'null')
-
+  expect(testReplyclean.data).not.toHaveProperty(
+    "todays_Attendance_Count",
+    "64"
+  );
+  expect(testReplyclean.data).not.toHaveProperty("todays_Absent_Count", "null");
 });
 
 // Get Engagement Score test
+test("Get engagement score test", async function () {
+  // Reset db
+  //   await resetTestDatabase();
+  // Run API call
+  const testReply = await fetch(
+    `${mainURL}${getRoute}getEngagementScoreByWeek?weekNumber=1`
+  );
+  // clean up response
+  // clean up response json
+  const testReplyJSON = await testReply.text();
+  const testReplyclean = JSON.parse(testReplyJSON);
+  console.log(testReplyclean);
+  // Check response object is type of
+  expect(testReplyclean).toHaveProperty("data");
+  // Check array 0 is John Doe
+  expect(testReplyclean.data[0]).toEqual({
+    name: "John Doe",
+    average_engagement_grade: "Ungraded",
+  });
+  expect(testReplyclean.data.length).toBe(9);
+
+  // Check success status
+  expect(testReplyclean).toHaveProperty("status", "success");
+
+  // Failing conditions
+  expect(testReplyclean.data).not.toHaveProperty(
+    "todays_Attendance_Count",
+    "64"
+  );
+  expect(testReplyclean.data[1]).not.toEqual({
+    name: "John Doe",
+    average_engagement_grade: "Ungraded",
+  });
+});
 
 // Delete name from namepicker test
 
